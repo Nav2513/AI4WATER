bf0490cb7aa8079f561f6b26b6d814a54f9c90c4
Nav2513 <134849961+Nav2513@users.noreply.github.com>
2025-05-06 08:39:28 +0530

    Defining authMiddleware and roleMiddleware

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
-
+const verifyToken = require('../middlewares/authMiddleware');
+const authorizeRoles = require('../middlewares/roleMiddleware');
 
 const router = express.Router();
 
 
 
 //Only admin can access
-router.get('/admin', (req, res) => {
+router.get('/admin', verifyToken, authorizeRoles("admin"), (req, res) => {
     res.json({ message: "Welcome Admin"});
 })
 
@@ -15,7 +16,7 @@ router.get('/admin', (req, res) => {
 
 // Both user and admin can access
 
-router.get('/user', (req, res) => {
+router.get('/user', verifyToken, authorizeRoles("user", "admin"), (req, res) => {
     res.json({ message: "Welcome user"});
 });
 
